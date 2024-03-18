3f144d8c35f88649a5e2f438f4f44be0376f55e1
rl-86 <rl86@live.se>
2024-03-18 22:43:51 +0100

    starting over

--- wordleAlgo.js
+++ wordleAlgo.js
@@ -1,33 +1,15 @@
-export default function compareWords(guess, secret) {
-  if (secret.every((element, index) => element === guess[index])) {
-    return 'Correct word!';
-  } else {
-    return 'Incorrect word!';
+export default function convertWord(inputWord) {
+  let letterObjects = [];
+  inputWord = inputWord.toUpperCase();
+  for (let i = 0; i < inputWord.length; i++) {
+    let letterObject = {
+      letter: inputWord[i],
+    };
+
+    letterObjects.push(letterObject);
   }
-}
-let secret = [];
-let guess = [];
-compareWords(guess, secret);
-/*
-export function secretWord(secret) {
-  let str1 = secret[0].toUpperCase().split('');
-  console.log(secret);
-  console.log(str1);
-  return str1;
+  return letterObjects;
 }
 
-export function guessedWord() {
-  let str2 = guess[0].toUpperCase().split('');
-  return str2;
-}
-
-export function compareWords() {
-  const matches = guess.map((char1) => {
-    return secret.some((char2) => char1 === char2);
-  });
-
-  return matches;
-}
-
-// compareWords(guess, secret);
-*/
+let wordObjects = convertWord('apple');
+console.log(wordObjects);
--- wordleAlgo.spec.js
+++ wordleAlgo.spec.js
@@ -1,48 +1,60 @@
 import { describe, expect, it } from '@jest/globals';
-import wordleAlgo from './wordleAlgo.js';
-import secretWord from './wordleAlgo.js';
-import guessedWord from './wordleAlgo.js';
-import compareWords from './wordleAlgo.js';
+//import wordleAlgo from './wordleAlgo.js';
+import convertWord from './wordleAlgo.js';
+import feedback from './wordleAlgo.js';
 
-describe('compareWords()', () => {
-  it('should compare guess with secret and return "Correct word!" if secret matches guess', () => {
-    const secret = ['A', 'P', 'P', 'L', 'E'];
-    const guess = ['A', 'P', 'P', 'L', 'E'];
-    const output = compareWords(guess, secret);
-    expect(output).toBe('Correct word!');
-  });
-
-  it('should compare guess with secret and return "Incorrect word!" if secret doesnt match guess', () => {
-    const secret = ['A', 'P', 'P', 'L', 'E'];
-    const guess = ['H', 'E', 'L', 'L', 'O'];
-    const output = compareWords(secret, guess);
-    expect(output).toBe('Incorrect word!');
+describe('convertWord()', () => {
+  it('should convert all letters of the word into individual objects and make them uppercase', () => {
+    const secretWord = 'apple';
+    const output = convertWord(secretWord);
+    expect(output).toEqual([
+      { letter: 'A' },
+      { letter: 'P' },
+      { letter: 'P' },
+      { letter: 'L' },
+      { letter: 'E' },
+    ]);
   });
 });
-
 /*
-describe('secretWord()', () => {
-  it('should split the secret word into letters and make them uppercase', () => {
-    const secret = ['aPPle'];
-    const output = secretWord(secret);
-    expect(output).toEqual(['A', 'P', 'P', 'L', 'E']);
+describe('feedback()', () => {
+  it('should compare the letters of the two words and return correct, incorrect or misplaced for each letter', () => {
+    const secretWord = 'APPLE';
+    const guessedWord = 'HELLO';
+    const output = feedback(secretWord, guessedWord);
+    expect(output).toEqual([
+      { letter: 'H', result: 'incorrect' },
+      { letter: 'E', result: 'missplaced' },
+      { letter: 'L', result: 'missplaced' },
+      { letter: 'L', result: 'correct' },
+      { letter: 'O', result: 'incorrect' },
+    ]);
   });
-});
 
-describe('guessedWord()', () => {
-  it('should split the guessed word into letters and make them uppercase', () => {
-    const guess = ['Hello'];
-    const output = guessedWord(guess);
-    expect(output).toEqual(['H', 'E', 'L', 'L', 'O']);
+  it('should compare the letters of the two words and return correct for each letter', () => {
+    const secretWord = 'APPLE';
+    const guessedWord = 'APPLE';
+    const output = feedback(secretWord, guessedWord);
+    expect(output).toEqual([
+      { letter: 'A', result: 'correct' },
+      { letter: 'P', result: 'correct' },
+      { letter: 'P', result: 'correct' },
+      { letter: 'L', result: 'correct' },
+      { letter: 'E', result: 'correct' },
+    ]);
   });
-});
 
-describe('compareWords()', () => {
-  it('should compare letters of both words and return an array of letters that match, true or false', () => {
-    const secret = ['A', 'P', 'P', 'L', 'E'];
-    const guess = ['H', 'E', 'L', 'L', 'O'];
-    const output = compareWords(guess, secret);
-    expect(output).toEqual([false, true, true, true, false]);
+  it('should compare the letters of the two words and return incorrect for each letter', () => {
+    const secretWord = 'APPLE';
+    const guessedWord = 'DIZZY';
+    const output = feedback(secretWord, guessedWord);
+    expect(output).toEqual([
+      { letter: 'D', result: 'incorrect' },
+      { letter: 'I', result: 'incorrect' },
+      { letter: 'Z', result: 'incorrect' },
+      { letter: 'Z', result: 'incorrect' },
+      { letter: 'Y', result: 'incorrect' },
+    ]);
   });
 });
 */
